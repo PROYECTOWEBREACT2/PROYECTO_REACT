a61b3f2a02b57116ca709d64daddc5715fa7bf06
LPAMENDEZ <panessoliliana785@gmail.com>
2025-11-28 14:01:13 -0500

    feach: actualizacion de cabecera

--- src/Componentes/CabeceraCV.jsx
+++ src/Componentes/CabeceraCV.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
+import mifoto from "../assets/foto_fondo_cv.jpeg";
 
 function CabeceraCV() {
   const data = {
     nombre: "Liliana Sofia Panesso Mendez",
     rol: "Desarrolladora Front-End Jr.",
-    telefono: "3007649182",
-    correo: "panessoliliana785@gmail.com",
-    ciudad: "Medellín, Colombia"
+    contacto: [
+      { icono: "fas fa-envelope", texto: "panessoliliana785@gmail.com" },
+      { icono: "fas fa-phone", texto: "3007649182" },
+      { icono: "fas fa-map-marker-alt", texto: "Medellín, Colombia" }
+    ]
   };
 
-  return  (
-  <>
-    
+  return (
     <header className="container">
- <div className="cabecera_cv">
-  <h1>{data.nombre}</h1>
-  <p className="info_contacto">
-    <span><i className="fas fa-envelope"></i> {data.correo}</span>
-    <span><i className="fas fa-phone"></i> {data.telefono}</span>
-    <span><i className="fas fa-map-marker-alt"></i> {data.ciudad}</span>
-  </p>
-  <h2 className="titulo_rol">{data.rol}</h2>
-</div>
+      <div className="cabecera_cv">
+        <div className="contenedor_foto_perfil">
+          <img src={mifoto} alt="Foto de perfil" />
+        </div>
 
-</header>
+        <h1>{data.nombre}</h1>
 
+        {/* Renderizado de lista de datos de contacto */}
+        <p className="info_contacto">
+          {data.contacto.map((item, index) => (
+            <span key={index}>
+              <i className={item.icono}></i> {item.texto}
+              {index < data.contacto.length - 1 && " | "}
+            </span>
+          ))}
+        </p>
 
-  </>
-);
-
+        <h2 className="titulo_rol">{data.rol}</h2>
+      </div>
+    </header>
+  );
 }
 
 export default CabeceraCV;
