0cb95a659cdcff31ffa3d6898d84dad3443ab00b
LPAMENDEZ <panessoliliana785@gmail.com>
2025-11-28 10:04:05 -0500

    : feat: componente CabeceraCV con datos personales actualizados

--- src/Componentes/CabeceraCV.jsx
+++ src/Componentes/CabeceraCV.jsx
@@ -1,1 +1,34 @@
-. 
+import React from 'react';
+
+function CabeceraCV() {
+  const data = {
+    nombre: "Liliana Sofia Panesso Mendez",
+    rol: "Desarrolladora Front-End Jr.",
+    telefono: "3007649182",
+    correo: "panessoliliana785@gmail.com",
+    ciudad: "Medellín, Colombia"
+  };
+
+  return  (
+  <>
+    
+    <header className="container">
+ <div className="cabecera_cv">
+  <h1>{data.nombre}</h1>
+  <p className="info_contacto">
+    <span><i className="fas fa-envelope"></i> {data.correo}</span>
+    <span><i className="fas fa-phone"></i> {data.telefono}</span>
+    <span><i className="fas fa-map-marker-alt"></i> {data.ciudad}</span>
+  </p>
+  <h2 className="titulo_rol">{data.rol}</h2>
+</div>
+
+</header>
+
+
+  </>
+);
+
+}
+
+export default CabeceraCV;
