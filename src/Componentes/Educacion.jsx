49914638eeb041860f091f440098357a19ce321f
LPAMENDEZ <panessoliliana785@gmail.com>
2025-11-28 10:52:29 -0500

    feat: componente Educacion con formación complementaria

--- src/Componentes/Educacion.jsx
+++ src/Componentes/Educacion.jsx
@@ -1,1 +1,34 @@
-. 
+import React from 'react';
+function Educacion() {
+  return (
+    <div className="container">
+      <section className="educacion">
+        <h3 className="titulo_Educacion">
+          <i className="fas fa-graduation-cap"></i> Formación Académica
+        </h3>
+
+        <h4>Análisis y Desarrollo de Software</h4>
+        <div className="Nombre_institucion">Instituto Tecnológico Censa</div>
+        <div className="anio_curso">2020–2024</div>
+        <br />
+
+        <h4>Diplomado en Diseño UX/UI</h4>
+        <div className="Nombre_institucion">Platzi</div>
+        <div className="anio_curso">2023</div>
+        <br />
+
+        <h4>Curso de Fundamentos de Programación</h4>
+        <div className="Nombre_institucion">Instituto Tecnológico Censa</div>
+        <div className="anio_curso">2020–2024</div>
+        <br />
+
+        <h4>Análisis y Desarrollo de Software</h4>
+        <div className="Nombre_institucion">Google Activate</div>
+        <div className="anio_curso">2022</div>
+        <br />
+      </section>
+    </div>
+  );
+}
+
+export default Educacion;
