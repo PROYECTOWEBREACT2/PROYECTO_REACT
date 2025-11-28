bb5d50e31222dd17ea905de560e881c8d852c84c
LPAMENDEZ <panessoliliana785@gmail.com>
2025-11-28 14:05:03 -0500

    feat: componente Educacion dinamico con formación complementaria

--- src/Componentes/Educacion.jsx
+++ src/Componentes/Educacion.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React from "react";
+
 function Educacion() {
+ 
+  const formacion = [
+    {
+      curso: "Análisis y Desarrollo de Software",
+      institucion: "Instituto Tecnológico Censa",
+      anio: "2020–2024",
+    },
+    {
+      curso: "Diplomado en Diseño UX/UI",
+      institucion: "Platzi",
+      anio: "2023",
+    },
+    {
+      curso: "Curso de Fundamentos de Programación",
+      institucion: "Google Activate",
+      anio: "2022",
+    },
+    {
+      curso: "Taller de Bases de Datos SQL",
+      institucion: "SENA",
+      anio: "2021",
+    },
+    {
+      curso: "Seminario de Seguridad Informática",
+      institucion: "Cisco Networking Academy",
+      anio: "2021",
+    },
+    {
+      curso: "Curso Complementario de HTML y CSS",
+      institucion: "FreeCodeCamp",
+      anio: "2022",
+    },
+  ];
+
   return (
     <div className="container">
       <section className="educacion">
@@ -7,25 +42,15 @@ function Educacion() {
           <i className="fas fa-graduation-cap"></i> Formación Académica
         </h3>
 
-        <h4>Análisis y Desarrollo de Software</h4>
-        <div className="Nombre_institucion">Instituto Tecnológico Censa</div>
-        <div className="anio_curso">2020–2024</div>
-        <br />
-
-        <h4>Diplomado en Diseño UX/UI</h4>
-        <div className="Nombre_institucion">Platzi</div>
-        <div className="anio_curso">2023</div>
-        <br />
-
-        <h4>Curso de Fundamentos de Programación</h4>
-        <div className="Nombre_institucion">Instituto Tecnológico Censa</div>
-        <div className="anio_curso">2020–2024</div>
-        <br />
-
-        <h4>Análisis y Desarrollo de Software</h4>
-        <div className="Nombre_institucion">Google Activate</div>
-        <div className="anio_curso">2022</div>
-        <br />
+        {/* Renderizado de listas */}
+        {formacion.map((item, index) => (
+          <div key={index} className="educacion_item">
+            <h4>{item.curso}</h4>
+            <div className="Nombre_institucion">{item.institucion}</div>
+            <div className="anio_curso">{item.anio}</div>
+            <br />
+          </div>
+        ))}
       </section>
     </div>
   );
