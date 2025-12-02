ad19e6ed019be84f6b2962112449f308e1a1ff8d
LPAMENDEZ <panessoliliana785@gmail.com>
2025-12-02 14:02:16 -0500

    feach: modificaciones sencillas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 
 import React from "react";
+
+import {
+  CabeceraData,
+  PerfilData,
+  EducacionData,
+  ExperienciaData,
+  StackTecnologiasData,
+} from "./data/cvData";
+
+
 import CabeceraCV from "./Componentes/CabeceraCV";
 import Perfil from "./Componentes/Perfil";
 import Educacion from "./Componentes/Educacion";
@@ -8,14 +18,18 @@ import StackTecnologias from "./Componentes/StackTecnologias";
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+
 function App() {
   return (
     <div >
-      <CabeceraCV />
-      <Perfil />
-      <Educacion />
-      <StackTecnologias />
-      <Experiencia />     
+      <CabeceraCV
+       nombre={CabeceraData.nombre}
+       rol={CabeceraData.rol}
+       contacto={CabeceraData.contacto} />
+      <Perfil texto={PerfilData.texto} />
+      <Educacion educacion={EducacionData.educacion}  />
+      <StackTecnologias tecnologias={StackTecnologiasData.tecnologias}  />
+      <Experiencia experiencias={ExperienciaData.experiencias}   />     
     
     </div>
   );
