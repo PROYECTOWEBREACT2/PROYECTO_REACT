62eb040fcf5fa6ddf3ab998470af428658faa3c4
LPAMENDEZ <panessoliliana785@gmail.com>
2025-12-02 19:35:12 -0500

    feat: componente Educación con props y desestructuracion

--- src/Componentes/Educacion.jsx
+++ src/Componentes/Educacion.jsx
@@ -1,39 +1,6 @@
 import React from "react";
 //function
-function Educacion() {
- 
-  const formacion = [
-    {
-      curso: "Análisis y Desarrollo de Software",
-      institucion: "Instituto Tecnológico Censa",
-      anio: "2020–2024",
-    },
-    {
-      curso: "Diplomado en Diseño UX/UI",
-      institucion: "Platzi",
-      anio: "2023",
-    },
-    {
-      curso: "Curso de Fundamentos de Programación",
-      institucion: "Google Activate",
-      anio: "2022",
-    },
-    {
-      curso: "Taller de Bases de Datos SQL",
-      institucion: "SENA",
-      anio: "2021",
-    },
-    {
-      curso: "Seminario de Seguridad Informática",
-      institucion: "Cisco Networking Academy",
-      anio: "2021",
-    },
-    {
-      curso: "Curso Complementario de HTML y CSS",
-      institucion: "FreeCodeCamp",
-      anio: "2022",
-    },
-  ];
+export default function  Educacion({educacion}) {
 
   return (
     <div className="container">
@@ -42,8 +9,8 @@ function Educacion() {
           <i className="fas fa-graduation-cap"></i> Formación Académica
         </h3>
 
-        {/* Renderizado de listas */}
-        {formacion.map((item, index) => (
+     
+        {educacion.map((item, index) => (
           <div key={index} className="educacion_item">
             <h4>{item.curso}</h4>
             <div className="Nombre_institucion">{item.institucion}</div>
@@ -56,4 +23,5 @@ function Educacion() {
   );
 }
 
-export default Educacion;
+
+
