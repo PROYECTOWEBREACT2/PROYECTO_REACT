4522dce7aa079b2978d08d64ab44f7f25be7130c
LPAMENDEZ <panessoliliana785@gmail.com>
2025-12-02 19:18:35 -0500

    feat: componente Perfil dinámico con props

--- src/Componentes/Perfil.jsx
+++ src/Componentes/Perfil.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-function Perfil() {
+export default function Perfil({texto}) {
   return (
     <div className="container">
       <>
         <div className="contenido_cv">
           <section className="habilidades">
             <h3 className="titulo_perfil"><i className="fas fa-user-circle"></i> Perfil Profesional</h3>
+           
             <br />
 
             <p className="carta_de_presentacion">
-              Soy aprendiz en análisis y desarrollo de software, con interés en crear soluciones digitales claras, funcionales y bien documentadas. Durante mi formación he trabajado en la elaboración de bitácoras técnicas, lo que me ha permitido organizar procesos de desarrollo y registrar aprendizajes de manera estructurada. También he fortalecido mis conocimientos en React, especialmente en el renderizado de componentes y el uso de técnicas como el destructuring, que facilitan la modularización y la eficiencia del código.
-              Me caracterizo por ser una persona metódica y curiosa, con disposición para aprender de los errores y transformar cada experiencia en una oportunidad de mejora. Valoro la claridad en la comunicación y la estética en la presentación de proyectos, buscando siempre que el resultado sea profesional y armónico.
-
+             {texto}
             </p>
 
             <br />
@@ -24,4 +23,4 @@ function Perfil() {
   );
 }
 
-export default Perfil;
+
